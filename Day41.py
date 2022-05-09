5d9e3018abe4eff8c6a7cbba46710003a04a1d95
Niloy <niloyman01@gmail.com>
2022-05-09 22:42:55 +0600

    Day 41 - HTML

---
+++ Day41.py
@@ -0,0 +1,15 @@
+# Day 41 - HTML
+
+# players = [172, 178, 180, 186, 190, 190, 192, 195, 200, 210]
+# # players = [14,18,19,24,26,33,42,55,67]
+# l = len(players)
+# mean = sum(players)/l
+# print(mean)
+# var = sum([(i-mean)**2 for i in players])/l
+# print(var)
+# std = var**0.5
+# print(std)
+# x = list(filter(lambda x: mean-std<=x<=mean+std, players))
+# print(x)
+# print(len(x))
+
